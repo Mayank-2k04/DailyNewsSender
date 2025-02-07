d78ed990e531e3dacea1d680c7d7fb15183072d6
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-02-07 19:01:16 +0530

    Add api key

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 
 url = "https://newsapi.org/v2/everything?q=tesla&from=2025-01-07&sortBy=publishedAt&apiKey=cf209f9437f04eae91144fcf67d258b3"
+api_key = 'cf209f9437f04eae91144fcf67d258b3'
 re = requests.get(url)
 content = re.json()
 for article in content['articles']:
