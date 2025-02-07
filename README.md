250601da1f807597db9451113a7f5c8979f2c387
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-02-07 18:13:01 +0530

    Acees news title and description using api

---
+++ README.md
@@ -0,0 +1,3 @@
+# What does this do ?
+
+Sends news related to a particular topic on email
---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "https://newsapi.org/v2/everything?q=tesla&from=2025-01-07&sortBy=publishedAt&apiKey=cf209f9437f04eae91144fcf67d258b3"
+re = requests.get(url)
+content = re.json()
+for article in content['articles']:
+    print(article['title'])
+    print(article['author'])
+    print(article['description'])
