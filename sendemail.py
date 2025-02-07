a0dbf77140408f7114a312f05c9e573bbf157d77
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-02-07 22:11:38 +0530

    Add subject

--- sendemail.py
+++ sendemail.py
@@ -5,22 +5,18 @@ host = "smtp.gmail.com"
 port = 465
 
 user = "mayank.kapoor2607@gmail.com"
-rec = "samshokeen04@gmail.com" #take user input
+ #take user input
 pa = "jwot zgaz ewxq vvhf"
 # Create SSL Context
 context = ssl.create_default_context()
 
-# Email Content (Include headers)
-headers = """\
-From: Mayank <mayank.kapoor2607@gmail.com>
-To: """
 # Send Email
 def sendmail(message,receiver,username = user,password=pa):
-    message = headers + receiver.split('@')[0] + " <" + receiver + ">\n" + message
+    message = f"From: Mayank <{user}>\nTo: {receiver.split('@')[0]} <{receiver}>\nSubject: Today's news\n{message}"
     try:
         with smtplib.SMTP_SSL(host, port, context=context) as server:
             server.login(username, password)
-            server.sendmail(username, receiver, message)
+            server.sendmail(username, receiver, message.encode('utf-8'))
         print("Email sent successfully!")
     except Exception as e:
         print("Error:", e)
