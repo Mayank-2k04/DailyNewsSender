679e4ddaa066e82c921c5b9dafe965c1508e6614
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-02-20 19:15:08 +0530

    add dynamic date feature, create env variable for news api key and convert code to a module

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import requests
 import sendemail
-topic = 'tesla'
-url = ("https://newsapi.org/v2/everything?"
-       f"q={topic}&"
-       "from=2025-01-07&"
-       "sortBy=publishedAt&"
-       "apiKey=cf209f9437f04eae91144fcf67d258b3&"
-       "language=en")
-api_key = 'cf209f9437f04eae91144fcf67d258b3'
-
-re = requests.get(url)
-content = re.json()
-print(content)
-message = ""
-i = 1
-
-for article in content['articles']:
-    if article['title'] is not None and article['description'] is not None:
-        message = message + (f"{i}  {str(article['title'])}"
-                             f" \n{str(article['description'])} \n"
-                             f" {article['url']}\n\n\n")
-        i += 1
-    if i == 10:
-        break
+import os
+from datetime import date
 
 
-print(message)
+def send_user_news(reci):
+    today = date.today()
+    topic = 'tesla'
+    api_key = os.getenv("NEWS_API_KEY")
+
+    url = ("https://newsapi.org/v2/everything?"
+           f"q={topic}&"
+           f"from={today}&"
+           "sortBy=publishedAt&"
+           f"apiKey={api_key}&"
+           "language=en")
+
+    re = requests.get(url)
+    content = re.json()
+    print(content)
+    message = ""
+
+    i = 0
+    for article in content['articles']:
+        if article['title'] is not None and article['description'] is not None:
+            message = message + (f"{i}  {str(article['title'])}"
+                                 f" \n{str(article['description'])} \n"
+                                 f" {article['url']}\n\n\n")
+            i += 1
+        if i == 10:
+            break
+    sendemail.sendmail(message, reci)
+
 rec = input("Enter receiver email : ")
-# sendemail.sendmail(message,rec)
+send_user_news(reci = rec)
+
